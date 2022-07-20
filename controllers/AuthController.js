4d07f4c43168cd4c299148fef6e7e1c5e034ed9d
Amr abdelfattah <amrabdelfattah98@gmail.com>
2022-07-20 20:03:31 +0200

    add product
    Signed-off-by: Amr abdelfattah <amrabdelfattah98@gmail.com>

---
+++ controllers/AuthController.js
@@ -0,0 +1,133 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const db = require("../lib/db");
+const { validateRequest } = require("../middleware/cus_sel");
+const router = require("express").Router();
+
+
+const register = async(req, res, next) => {
+    db.query(
+        `SELECT * FROM customer WHERE email = ${db.escape(req.body.email)};`,
+        (err, result) => {
+          if (result.length) {
+            return res.status(409).send({
+              status: 400,
+              message: "This email is already in use!",
+            });
+          } else {
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+              if (err) {
+                return res.status(500).send({
+                  msg: err,
+                });
+              } else {
+                let name = req.body.name;
+                let email = req.body.email;
+                // let password = req.body.password;
+                // const encryptedPassword = await bcrypt.hash(password, 10)
+
+                var data = {
+                  name: name,
+                  email: email,
+                  password: hash,
+                };
+                var sql = "insert into customer SET ?;";
+                db.query(sql, data, (err) => {
+                  if (!err) {
+                    res.send({
+                      status: 200,
+                      message: " registered sucessfully",
+                    });
+                  } else
+                    res.send({
+                      status: 400,
+                      message: "error occurred",
+                      error: error,
+                    });
+                });
+              }
+            });
+          }
+        }
+      );
+
+}
+const login = (req, res, next) => {
+    db.query(
+        `SELECT * FROM customer WHERE email = ${db.escape(req.body.email)};`,
+        (err, result) => {
+          if (err) {
+            throw err;
+            return res.status(400).send({
+              msg: err,
+            });
+          }
+          if (!result.length) {
+            return res.status(401).send({
+              msg: "Email or password is incorrect!",
+            });
+          }
+          bcrypt.compare(
+            req.body.password,
+            result[0]["password"],
+            (bErr, bResult) => {
+              if (bErr) {
+                throw bErr;
+                return res.status(401).send({
+                  msg: "Email or password is incorrect!",
+                });
+              }
+              if (bResult) {
+                const token = jwt.sign(
+                  {
+                    username: result[0].name,
+                    userId: result[0].cus_id,
+                    email: result[0].email,
+                  },
+                  "363636",
+                  {
+                    expiresIn: "1d",
+                  }
+                );
+                db.query(
+                  `UPDATE customer SET last_login = now() WHERE cus_id = '${result[0].cus_id}'`
+                );
+                // return res
+                //   .cookie("jwt", token, {
+                //     httpOnly: true,
+                //     maxAge: 60*24,
+                //     secure: process.env.NODE_ENV === "production",
+                //   })
+                //   .status(200)
+                //   .json({ message: "Logged in successfully 😊 👌" });
+
+                return res.status(200).send({
+                  msg: "Logged in successfully 😊 👌",
+                  token
+                });
+              }
+              return res.status(401).send({
+                msg: "email or password is incorrect!",
+              });
+            }
+          );
+        }
+      );
+
+}
+const logout = (req, res, next) => {
+
+
+    res.cookie('jwt', '', {maxAge: 0})
+    res.send({
+        message: 'success logout'
+    })
+}
+const test = (req, res, next) => {
+    console.log(req.userData);
+    res.send("TOnly logged in users can see that!");
+
+}
+
+
+module.exports = {login,logout ,test ,register }
--- routes/routes.js
+++ routes/routes.js
@@ -8,140 +8,147 @@ const { validateRequest } = require("../middleware/cus_sel");
 const cust_selMiddleware = require("../middleware/cus_sel");
 const order_controller = require('../controllers/order_controler');
 const product_controller = require('../controllers/product');
+const AuthController = require('../controllers/AuthController');
 
 
 
 
-router.post("/register", validateRequest, async (req, res, next) => {
-  db.query(
-    `SELECT * FROM customer WHERE email = ${db.escape(req.body.email)};`,
-    (err, result) => {
-      if (result.length) {
-        return res.status(409).send({
-          status: 400,
-          message: "This email is already in use!",
-        });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).send({
-              msg: err,
-            });
-          } else {
-            let name = req.body.name;
-            let email = req.body.email;
-            // let password = req.body.password;
-            // const encryptedPassword = await bcrypt.hash(password, 10)
+// router.post("/register", validateRequest, async (req, res, next) => {
+//   db.query(
+//     `SELECT * FROM customer WHERE email = ${db.escape(req.body.email)};`,
+//     (err, result) => {
+//       if (result.length) {
+//         return res.status(409).send({
+//           status: 400,
+//           message: "This email is already in use!",
+//         });
+//       } else {
+//         bcrypt.hash(req.body.password, 10, (err, hash) => {
+//           if (err) {
+//             return res.status(500).send({
+//               msg: err,
+//             });
+//           } else {
+//             let name = req.body.name;
+//             let email = req.body.email;
+//             // let password = req.body.password;
+//             // const encryptedPassword = await bcrypt.hash(password, 10)
 
-            var data = {
-              name: name,
-              email: email,
-              password: hash,
-            };
-            var sql = "insert into customer SET ?;";
-            db.query(sql, data, (err) => {
-              if (!err) {
-                res.send({
-                  status: 200,
-                  message: " registered sucessfully",
-                });
-              } else
-                res.send({
-                  status: 400,
-                  message: "error occurred",
-                  error: error,
-                });
-            });
-          }
-        });
-      }
-    }
-  );
-});
+//             var data = {
+//               name: name,
+//               email: email,
+//               password: hash,
+//             };
+//             var sql = "insert into customer SET ?;";
+//             db.query(sql, data, (err) => {
+//               if (!err) {
+//                 res.send({
+//                   status: 200,
+//                   message: " registered sucessfully",
+//                 });
+//               } else
+//                 res.send({
+//                   status: 400,
+//                   message: "error occurred",
+//                   error: error,
+//                 });
+//             });
+//           }
+//         });
+//       }
+//     }
+//   );
+// });
 
-router.post("/login", (req, res, next) => {
-  db.query(
-    `SELECT * FROM customer WHERE email = ${db.escape(req.body.email)};`,
-    (err, result) => {
-      if (err) {
-        throw err;
-        return res.status(400).send({
-          msg: err,
-        });
-      }
-      if (!result.length) {
-        return res.status(401).send({
-          msg: "Email or password is incorrect!",
-        });
-      }
-      bcrypt.compare(
-        req.body.password,
-        result[0]["password"],
-        (bErr, bResult) => {
-          if (bErr) {
-            throw bErr;
-            return res.status(401).send({
-              msg: "Email or password is incorrect!",
-            });
-          }
-          if (bResult) {
-            const token = jwt.sign(
-              {
-                username: result[0].name,
-                userId: result[0].cus_id,
-                email: result[0].email,
-              },
-              "363636",
-              {
-                expiresIn: "1d",
-              }
-            );
-            db.query(
-              `UPDATE customer SET last_login = now() WHERE cus_id = '${result[0].cus_id}'`
-            );
-            // return res
-            //   .cookie("jwt", token, {
-            //     httpOnly: true,
-            //     maxAge: 60*24,
-            //     secure: process.env.NODE_ENV === "production",
-            //   })
-            //   .status(200)
-            //   .json({ message: "Logged in successfully 😊 👌" });
+// router.post("/login", (req, res, next) => {
+//   db.query(
+//     `SELECT * FROM customer WHERE email = ${db.escape(req.body.email)};`,
+//     (err, result) => {
+//       if (err) {
+//         throw err;
+//         return res.status(400).send({
+//           msg: err,
+//         });
+//       }
+//       if (!result.length) {
+//         return res.status(401).send({
+//           msg: "Email or password is incorrect!",
+//         });
+//       }
+//       bcrypt.compare(
+//         req.body.password,
+//         result[0]["password"],
+//         (bErr, bResult) => {
+//           if (bErr) {
+//             throw bErr;
+//             return res.status(401).send({
+//               msg: "Email or password is incorrect!",
+//             });
+//           }
+//           if (bResult) {
+//             const token = jwt.sign(
+//               {
+//                 username: result[0].name,
+//                 userId: result[0].cus_id,
+//                 email: result[0].email,
+//               },
+//               "363636",
+//               {
+//                 expiresIn: "1d",
+//               }
+//             );
+//             db.query(
+//               `UPDATE customer SET last_login = now() WHERE cus_id = '${result[0].cus_id}'`
+//             );
+//             // return res
+//             //   .cookie("jwt", token, {
+//             //     httpOnly: true,
+//             //     maxAge: 60*24,
+//             //     secure: process.env.NODE_ENV === "production",
+//             //   })
+//             //   .status(200)
+//             //   .json({ message: "Logged in successfully 😊 👌" });
 
-            return res.status(200).send({
-              msg: "Logged in successfully 😊 👌",
-              token
-            });
-          }
-          return res.status(401).send({
-            msg: "email or password is incorrect!",
-          });
-        }
-      );
-    }
-  );
-});
+//             return res.status(200).send({
+//               msg: "Logged in successfully 😊 👌",
+//               token
+//             });
+//           }
+//           return res.status(401).send({
+//             msg: "email or password is incorrect!",
+//           });
+//         }
+//       );
+//     }
+//   );
+// });
 
-router.get("/test", cust_selMiddleware.isLoggedIn, (req, res, next) => {
-  console.log(req.userData);
-  res.send("TOnly logged in users can see that!");
-});
+// router.get("/test", cust_selMiddleware.isLoggedIn, (req, res, next) => {
+//   console.log(req.userData);
+//   res.send("TOnly logged in users can see that!");
+// });
 
-router.put("/logout", cust_selMiddleware.isLoggedIn, function (req, res) {
+// router.put("/logout", cust_selMiddleware.isLoggedIn, function (req, res) {
 
-        res.cookie('jwt', '', {maxAge: 0})
-        res.send({
-            message: 'success logout'
-        })
-});
+//         res.cookie('jwt', '', {maxAge: 0})
+//         res.send({
+//             message: 'success logout'
+//         })
+// });
 
 
+router.post("/register", validateRequest, AuthController.register);
+router.post("/login",  AuthController.login);
+router.put("/logout",  cust_selMiddleware.isLoggedIn, AuthController.logout);
+router.get("/test", cust_selMiddleware.isLoggedIn, AuthController.test);
 
 
 router.post('/order',cust_selMiddleware.isLoggedIn, order_controller.neworder);
 router.get('/get_order',cust_selMiddleware.isLoggedIn, order_controller.getorder);
 router.delete('/delete_order/:ord_id',cust_selMiddleware.isLoggedIn, order_controller.deleteorder);
 router.delete('/delete_all',cust_selMiddleware.isLoggedIn, order_controller.deleteall);
+
+
 
 
 router.post('/create',cust_selMiddleware.isLoggedIn, product_controller.createproduct);
